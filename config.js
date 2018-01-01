5bc51202a5bc758eb9f587b72c6fab52f8dab89b
Tchekda <tchekda@gmail.com>
2018-01-01 19:32:54 +0100

    Default Version of the configuration

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
-module.exports = {
-  consumer_key:         'jOsVyauB3XM4AvlJzF3JbdbRx',
-  consumer_secret:      'AcDkRg0uos0JBJ8RWLbtBP2lBFDqAayK2sObpJMsTUHxoYUKGt',
-  access_token:         '883737566381473792-Gr2ejQgcFxaJH5sLWHNpl3HwoxVb7Qc',
-  access_token_secret:  'nx2WX80mWKed2J6r7GBEIthUEWXC9IPMgJDsfAyyp5ydn',
-  timeout_ms:           60*1000,
-  start:                'Chargement du bot!',
-  stop:                 'Extiction du bot!',
-  user:                 'Tchekda',
-  welcome_message:      'Bienvenue sur le compte de @Tchekda'
-}
+var data = {
+    consumer_key:           'CONSUMER_KEY',
+    consumer_secret:        'CONSUMER_SECRET',
+    access_token:           'ACCESS_TOKEN',
+    access_token_secret:    'ACCESS_TOKEN_SECRET',
+    timeout_ms:             60 * 1000,
+    user:                   'USERNAME'
+};
+
+var log = {
+    start:                  'Loading....',
+    stop:                   'Shutting down...',
+    welcome_message:        'Welcome to my account'
+};
+
+module.exports={data:data,log:log};
