904d907d29d2e6e5ae208ffa9a95bf5f6e83767c
Tchekda <tchekda@gmail.com>
2018-01-01 18:38:57 +0100

    Refonte totale de l'organisation des fonctions

--- bot.js
+++ bot.js
@@ -1,22 +1,24 @@
 var config = require('./config.js');
 var Twit = require('twit');
-var T = new Twit(config);
-var stream = T.stream('user');
+var eventModule = require('./twitter-event.js');
+var twit = new Twit(config);
+var stream = twit.stream('user');
 var stdin = process.openStdin();
-var tweetID;
+
+eventModule.init(config, twit);
 
 console.log(config.start);
 setWelcome();
 
-stream.on('tweet', tweeted);
+stream.on('tweet', identified);
 stream.on('follow', followed);
 stream.on('quoted_tweet', quoted);
-stream.on('direct_message', messaged);
+stream.on('direct_message', receiveMessage);
 
 
-stdin.addListener("data", function(d) {
+stdin.addListener("data", function (d) {
     var input = d.toString().trim();
-    switch(input.split(' ')[0]){
+    switch (input.split(' ')[0]) {
         case 'stop':
             console.log(config.stop);
             process.exit();
@@ -28,173 +30,5 @@ stdin.addListener("data", function(d) {
         default:
             console.log('Commande inconnue!');
     }
-  });
-
-function tweeted(event){
-    var replyto = event.in_reply_to_screen_name;
-    var from = event.user.screen_name;
-
-    if(replyto === config.user){
-        likeIt(event.id_str, from);
-    }
-}
-
-function followed(event){
-    var sourceName = event.source.name;
-    var screenName = event.source.screen_name;
-    console.log(event);
-    if(screenName !== config.user){
-        console.log(sourceName + ' a commencé à vous suivre : @' + screenName);
-        tweetIt('@' + screenName + ' merci du follow! #Subscribe');
-    }
-}
-
-function quoted(event){
-    var replyto = event.source.screen_name;
-    var from = event.target_object.user.screen_name;
-
-    if(replyto !== config.user){
-        likeIt(event.target_object.id_str, event.target_object.user.screen_name);
-    }
-}
-
-function messaged(event){
-    var to = event.direct_message.recipient.screen_name;
-    var splited = event.direct_message.text.toLowerCase().split(' ');
-    if(to === config.user){
-        if(splited.indexOf('bot') !== -1){
-            //Contient bot
-        }
-    }}
-
-function tweetIt(text){
-    var params = {
-        status : text
-    };
-
-    function logData(err, data, response){
-        if(err){
-            console.log('Une erreure est survenue : ' + err)
-        }else{
-           console.log('Votre Tweet à bien été envoyé : ' + text);
-        }
-    }
-
-    T.post('statuses/update', params, logData)
-}
-
-function replaceTweet(text){
-    getTweetId(text, function(tweetId){
-        if(tweetId === null){
-            tweetIt(config.start);
-        }else{
-            deleteTweet(tweetId);
-        }
-    });
-
-}
-
-function getTweetId(text, fonction){
-    var params = {
-        q : text + ' from:' + config.user
-    };
-    T.get('search/tweets', params, receiveTweet);
-    function receiveTweet(err, data, response){
-
-        if(err){
-            console.log('Une erreure est survenue : ' + err)
-        }else{
-            console.log(data);
-            if(data.statuses.length === 0){
-                tweetID = null;
-            }else{
-                console.log(data.statuses.length);
-                tweetID = data.statuses[0].id_str;
-            }
-            fonction(tweetID);
-        }
-    }
-
-}
-
-function deleteTweet(tweetId){
-        var params = {
-            id : tweetId
-        };
-        T.post('statuses/destroy', params, deleted);
-        function deleted(err, data, response){
-            if(err){
-                console.log('Une erreure est survenue : ' + err)
-            }else{
-                tweetIt(config.start);
-            }
-        }
-    }
-
-function likeIt(tweetId, name){
-    var params = {
-        id : tweetId
-    };
-
-    T.post('favorites/create', params, liked);
-
-    function liked(err, data, response){
-        if(err){
-            console.log('Une erreure est survenue : ' + err)
-        }else{
-           console.log('Le Tweet de @' + name + ' a bien été liké!');
-        }
-    }
-}
-
-function sendIt(name, msg){
-    var params = {
-        id : tweetId
-    };
-
-    T.post('favorites/create', params, liked);
-
-    function liked(err, data, response){
-        if(err){
-            console.log('Une erreure est survenue : ' + err)
-        }else{
-           console.log('Le Tweet de @' + name + ' a bien été liké!');
-        }
-    }
-}
-function setWelcome(){
-    var params = {
-      "welcome_message": {
-        "message_data": {
-          "text": config.welcome_message,
-          "ctas": [
-            {
-              "type": "web_url",
-              "label": "Ma chaine YouTube",
-              "url": "https://www.youtube.com/channel/UCx5CZE6SSovbUK1dvlylJkA"
-            },
-            {
-              "type": "web_url",
-              "label": "Mon site web",
-              "url": "https://www.codeschool.fr"
-            },
-            {
-              "type": "web_url",
-              "label": "Abonner",
-              "url": "https://twitter.com/intent/follow?screen_name=tchekda"
-            }
-          ]
-        }
-      }
-    };
-
-    T.post('direct_messages/welcome_messages/new', params, sended);
-
-    function sended(err, data, response){
-        if(err){
-            console.log('Une erreure est survenue : ' + err)
-        }else{
-           console.log('Le message d\'accueil a bien été définis : ' + params.welcome_message.message_data.text);
-        }
-    }
-}
+});
+function identified(e){eventModule.identified(e)}function followed(e){eventModule.followed(e)}function quoted(e){eventModule.quoted(e)}function receiveMessage(e){eventModule.receiveMessage(e)}function tweetIt(e){eventModule.tweetText(e)}function setWelcome(){eventModule.setWelcomeMessage()}
---
+++ twitter-event.js
@@ -0,0 +1,162 @@
+var config = null;
+var twit = null;
+
+var init = function (configuration, twitter) {
+    config = configuration;
+    twit = twitter;
+};
+
+var identified = function (event) {
+    var replyto = event.in_reply_to_screen_name;
+    var from = event.user.screen_name;
+
+    if(replyto === config.user){
+        likeTweet(event.id_str, from);
+    }
+};
+
+var followed = function (event) {
+    var sourceName = event.source.name;
+    var screenName = event.source.screen_name;
+    if(screenName !== config.user){
+        console.log(sourceName + ' a commencé à vous suivre : @' + screenName);
+        tweetText('@' + screenName + ' merci du follow! #Subscribe');
+    }
+};
+
+var quoted = function (event) {
+    var replyto = event.source.screen_name;
+
+    if(replyto !== config.user){
+        likeTweet(event.target_object.id_str, event.target_object.user.screen_name);
+    }
+};
+
+var tweetText = function (text) {
+    var params = {
+        status : text
+    };
+
+    function logData(err){
+        if(err){
+            console.log('Une erreure est survenue : ' + err)
+        }else{
+            console.log('Votre Tweet à bien été envoyé : ' + text);
+        }
+    }
+
+    twit.post('statuses/update', params, logData)
+};
+
+var setWelcomeMessage = function () {
+    var params = {
+        "welcome_message": {
+            "message_data": {
+                "text": config.welcome_message,
+                "ctas": [
+                    {
+                        "type": "web_url",
+                        "label": "Ma chaine YouTube",
+                        "url": "https://www.youtube.com/channel/UCx5CZE6SSovbUK1dvlylJkA"
+                    },
+                    {
+                        "type": "web_url",
+                        "label": "Mon site web",
+                        "url": "https://www.codeschool.fr"
+                    },
+                    {
+                        "type": "web_url",
+                        "label": "Abonner",
+                        "url": "https://twitter.com/intent/follow?screen_name=tchekda"
+                    }
+                ]
+            }
+        }
+    };
+
+    twit.post('direct_messages/welcome_messages/new', params, sended);
+
+    function sended(err){
+        if(err){
+            console.log('Une erreure est survenue : ' + err)
+        }else{
+            console.log('Le message d\'accueil a bien été définis : ' + params.welcome_message.message_data.text);
+        }
+    }
+};
+
+var likeTweet = function (tweetID, authorName) {
+    var params = {
+        id : tweetID
+    };
+
+    twit.post('favorites/create', params, liked);
+
+    function liked(err){
+        if(err){
+            console.log('Une erreure est survenue : ' + err)
+        }else{
+            console.log('Le Tweet de @' + authorName + ' a bien été liké!');
+        }
+    }
+};
+
+var deleteTweet = function (tweetID) {
+    var params = {
+        id: tweetID
+    };
+    twit.post('statuses/destroy', params, deleted);
+
+    function deleted(err) {
+        if (err) {
+            console.log('Une erreure est survenue : ' + err)
+        } else {
+            console.log('Le Tweet à bien été supprimé');
+        }
+    }
+};
+
+var receiveMessage = function (event) {
+    var to = event.direct_message.recipient.screen_name;
+    var splited = event.direct_message.text.toLowerCase().split(' ');
+    if (to === config.user) {
+        if (splited.indexOf('bot') !== -1) {
+            //Contient bot
+        }
+    }
+};
+
+var getTweetID = function (text) {
+    var params = {
+        q: text + ' from:' + config.user
+    };
+    twit.get('search/tweets', params, receiveTweet);
+
+    function receiveTweet(err, data) {
+
+        if (err) {
+            console.log('Une erreure est survenue : ' + err)
+            tweetID = null;
+        } else {
+            console.log(data);
+            if (data.statuses.length === 0) {
+                tweetID = null;
+            } else {
+                console.log(data.statuses.length);
+                tweetID = data.statuses[0].id_str;
+            }
+        }
+        return tweetID;
+    }
+}
+
+module.exports = {
+    init:               init,
+    setWelcomeMessage:  setWelcomeMessage,
+    tweetText:          tweetText,
+    quoted:             quoted,
+    identified:         identified,
+    followed:           followed,
+    receiveMessage:     receiveMessage,
+}
+
