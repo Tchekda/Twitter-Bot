a2900ee943a7e0dcd3b5135b6096f11aa5f37257
Tchekda <tchekda@gmail.com>
2018-01-01 23:13:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,21 @@
 var data = {
-    consumer_key:           'CONSUMER_KEY',
-    consumer_secret:        'CONSUMER_SECRET',
-    access_token:           'ACCESS_TOKEN',
-    access_token_secret:    'ACCESS_TOKEN_SECRET',
+    consumer_key:           'jOsVyauB3XM4AvlJzF3JbdbRx',
+    consumer_secret:        'AcDkRg0uos0JBJ8RWLbtBP2lBFDqAayK2sObpJMsTUHxoYUKGt',
+    access_token:           '883737566381473792-Gr2ejQgcFxaJH5sLWHNpl3HwoxVb7Qc',
+    access_token_secret:    'nx2WX80mWKed2J6r7GBEIthUEWXC9IPMgJDsfAyyp5ydn',
     timeout_ms:             60 * 1000,
-    user:                   'USERNAME'
+    user:                   'Tchekda'
 };
 
 var log = {
-    start:                  'Loading....',
-    stop:                   'Shutting down...',
-    welcome_message:        'Welcome to my account'
+    logDir:             '/var/log/twitter/',
+    logInfoFile:        'info.log',
+    logErrorFile:       'error.log'
 };
 
-module.exports={data:data,log:log};
+var messages = {
+    start:                  'Chargement du bot',
+    stop:                   'Extiction du bot!',
+    welcome_message:        'Bienvenue sur le compte de @Tchekda'
+};
+module.exports={data:data,log:log,messages:messages};
