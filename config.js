d74e41e50a5679a96cb9566eb1cbd5f27c70ba93
Tchekda <tchekda@gmail.com>
2018-01-01 14:05:42 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  consumer_key:         'jOsVyauB3XM4AvlJzF3JbdbRx',
+  consumer_secret:      'AcDkRg0uos0JBJ8RWLbtBP2lBFDqAayK2sObpJMsTUHxoYUKGt',
+  access_token:         '883737566381473792-Gr2ejQgcFxaJH5sLWHNpl3HwoxVb7Qc',
+  access_token_secret:  'nx2WX80mWKed2J6r7GBEIthUEWXC9IPMgJDsfAyyp5ydn',
+  timeout_ms:           60*1000,
+  start:                'Chargement du bot!',
+  stop:                 'Extiction du bot!',
+  user:                 'Tchekda',
+  welcome_message:      'Bienvenue sur le compte de @Tchekda'
+}
