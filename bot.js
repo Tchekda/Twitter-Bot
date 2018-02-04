dbe6b04d2cc007746f906100700faaa4b0d684ef
Tchekda <tchekda@gmail.com>
2018-02-04 23:33:12 +0100

    Revert "Restructuration of the variable definition"
    This reverts commit 39586baa1039cd44c1fb8ee77d35be9e7a3355e0.

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 //Initialisation of all variables needed in the bot
-try {
 var config = require('./config.js'),                //Load configuration
     Twit = require('twit'),                         //Load Twitter API
     eventModule = require('./twitter-event.js'),    //Load functions on Twitter API
@@ -14,21 +13,6 @@ var config = require('./config.js'),                //Load configuration
     optsError = {logFilePath: config.log.logDir + config.log.logErrorFile, timestampFormat: "YYYY-MM-DD HH:mm:ss"}, //options of Error Logger
     logInfo = SimpleNodeLogger.createSimpleLogger(optsInfo), //Define Info Logger
     logError = SimpleNodeLogger.createSimpleLogger(optsError); //Define Error Logger
-} catch (error) {
-    logError.error('An error occured : ' + twitError)      
-  }
-// Creation of logging files and directory
-if (!fs.existsSync(config.log.logDir)) { //If log Directory does not exist
-    try {
-        fs.mkdirSync(config.log.logDir) //Create It
-    } catch (exception) {
-        console.log('Can\'t create the following directory : ' + config.log.logDir)
-        process.exit() // Quit to prevent crash
-    }
-    createFiles() //So create log files
-} else if (!fs.existsSync(config.log.logDir + config.log.logInfoFile) || !fs.existsSync(config.log.logDir + config.log.logErrorFile)) { //If one of the log files doesn't exist
-    createFiles() //Create them
-}
 
 var infos = { //Define data for twitter-event.js
     config: config,
@@ -36,55 +20,64 @@ var infos = { //Define data for twitter-event.js
     errorLog: logError,
     infoLog: logInfo
 };
-eventModule.init(infos) //Initializing data to twitter-event.js
+eventModule.init(infos); //Initializing data to twitter-event.js
 
-
+// Creation of logging files and directory
+if (!fs.existsSync(config.log.logDir)) { //If log Directory does not exist
+    try {
+        fs.mkdirSync(config.log.logDir); //Create It
+    } catch (exception) {
+        console.log('Can\'t create the following directory : ' + config.log.logDir);
+        process.exit(); // Quit to prevent crash
+    }
+    createFiles(); //So create log files
+} else if (!fs.existsSync(config.log.logDir + config.log.logInfoFile) || !fs.existsSync(config.log.logDir + config.log.logErrorFile)) { //If one of the log files doesn't exist
+    createFiles(); //Create them
+}
 
 function createFiles() {
     fs.writeFile(config.log.logDir + config.log.logInfoFile, "", function (o) { //Create info Log file
         if (o) {
-            console.log('Can\'t create the following file : ' + config.log.logDir + config.log.logInfoFile)
-            throw o
+            console.log('Can\'t create the following file : ' + config.log.logDir + config.log.logInfoFile);
+            throw o;
         }
         logInfo.info(config.log.logInfoFile + " has been created")
-    })
+    });
     fs.writeFile(config.log.logDir + config.log.logErrorFile, "", function (o) { //Create Error Log file
         if (o) {
-            console.log('Can\'t create the following file : ' + config.log.logDir + config.log.logErrorFile)
-            throw o
+            console.log('Can\'t create the following file : ' + config.log.logDir + config.log.logErrorFile);
+            throw o;
         }
         logInfo.info(config.log.logErrorFile + " has been created")
-    })
+    });
 }
 
 
-logInfo.info(config.console.start) //Log init bot = All seems good
+logInfo.info(config.console.start); //Log init bot = All seems good
 
 
 //Setting up all streams
-userStream.on('tweet', eventModule.identified)
-userStream.on('follow', eventModule.followed)
-userStream.on('quoted_tweet', eventModule.quoted)
-userStream.on('direct_message', eventModule.receiveMessage)
-publicStream.on('tweet', eventModule.retweetKeyWords)
+userStream.on('tweet', eventModule.identified);
+userStream.on('follow', eventModule.followed);
+userStream.on('quoted_tweet', eventModule.quoted);
+userStream.on('direct_message', eventModule.receiveMessage);
+publicStream.on('tweet', eventModule.retweetKeyWords);
 
 //Define listener of console commands
 stdin.addListener("data", function (d) {
-    var command = d.toString().trim().split(' ')[0],
-        args = d.toString().trim().split(' ');
-    switch (command) {
+    var input = d.toString().trim();
+    switch (input.split(' ')[0]) {
         case 'stop':
-            logInfo.info(config.console.stop)
-            setTimeout(function() { process.exit() }, 1); //To see the previous log
-            break
+            console.log(config.console.stop);
+            process.exit();
+            break;
         case 'clear':
-            process.stdout.write("\u001b[2J\u001b[00H")
-            console.log('Cleared!')
-            break
+            process.stdout.write("\u001b[2J\u001b[0;0H");
+            console.log('Cleared!');
+            break;
         default:
-            logError.error('Unknown Command : ' + command)
-            break
+            console.log('Unknown Command');
     }
-})
+});
 
-eventModule.setWelcomeMessage() //Define Welcome Message on the account = All good
+eventModule.setWelcomeMessage(); //Define Welcome Message on the account = All good
--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 var data = { //Define all data for Twitter API "https://apps.twitter.com"
-consumer_key:           'jm870cDLE9oNp7eA33OGv1QrV',
-consumer_secret:        'pOexjPuQ37Wz3UTkbB4uM0g6BP3IPriDXvxFAzbxSuF3kfTjIJ',
-access_token:           '883737566381473792-Gr2ejQgcFxaJH5sLWHNpl3HwoxVb7Qc',
-access_token_secret:    'nx2WX80mWKed2J6r7GBEIthUEWXC9IPMgJDsfAyyp5ydn',
+    consumer_key:           '',
+    consumer_secret:        '',
+    access_token:           '',
+    access_token_secret:    '',
     timeout_ms:             60 * 1000,
     username:               '', //Your @username
     userID:                 '', //Your Owner ID
     retweetWords: [] //Keywords to retweet
-}
+};
 
 var log = { //Define all Data for the Logger
     logDir:             'log/',
     logInfoFile:        'info.log',
     logErrorFile:       'error.log'
-}
+};
 
 var messages = { //Define custom messages
     welcome_message:        'Welcome in my DM\'s',
     new_follower:           '@%username% has just followed me!'
-}
+};
 
 var console = {
     start:                  'Bot Loading...',
@@ -32,4 +32,4 @@ var console = {
 }
 
 //Export all data to the main module (bot.js)
-module.exports={data:data,log:log,messages:messages,console:console}
+module.exports={data:data,log:log,messages:messages,console:console};
